2be592c422f9589800ec1b55a67df976f610d5b7
boy338 <boy.338@hotmail.com>
2014-07-05 04:13:30 +0700

    แก้ไขการสร้างเอกสาร docx

--- application/controllers/managewarranty.php
+++ application/controllers/managewarranty.php
@@ -103,7 +103,7 @@ class Managewarranty extends CI_Controller {
 		$PHPWord = new PHPWord();
 		$PHPWord->setDefaultFontName('TH SarabunPSK');
 		$PHPWord->setDefaultFontSize(16);
-		$PHPWord->addFontStyle('HeadStyle', array('bold'=>true));
+		$PHPWord->addFontStyle('HeadStyle', array('bold'=>true,'size'=>18));
 		
 		$HeadTables=array('spacing'=>0,'spaceBefore'=>0,'spaceAfter'=>0,'align'=>'center');
 		$ContentTables=array('spacing'=>0,'spaceBefore'=>0,'spaceAfter'=>0);
@@ -115,59 +115,104 @@ class Managewarranty extends CI_Controller {
 		
 		// New portrait section
 		$section = $PHPWord->createSection();
+		
 		$section->addImage('images/garuda_logo.png',array('width'=>100, 'height'=>100, 'align'=>'center'));
 		$section->addText('คำรับรองการปฏิบัติราชการ', 'HeadStyle',array('align'=>'center','spacing'=>0,'spaceBefore'=>0,'spaceAfter'=>0));
 		$section->addText('กรมยุโรป', 'HeadStyle', array('align'=>'center','spacing'=>0,'spaceBefore'=>0,'spaceAfter'=>0));
 		$section->addText('ประจำปีงบประมาณ พ.ศ. 2556', 'HeadStyle', array('align'=>'center','spacing'=>0,'spaceBefore'=>0,'spaceAfter'=>0));
-		$section->addTextBreak(1);
+		$section->addTextBreak(2);
 		
 		$section->addText('1. คำรับรองระหว่าง ');
 		
+		$section->addText('');
+		
 		$table = $section->addTable();
 		$table->addRow();
-		$table->addCell(6000)->addText('นายสีหศักดิ์ พวงเกตุแก้ว');
-		$table->addCell(6000)->addText('ปลัดกระทรวงต่างประเทศ');
-		$table->addCell(6000)->addText('ผู้รับคำรับรอง');
-		
-		//$section->addText('และ');
-		$table->addRow();
-		$table->addCell(180000, array('gridSpan' => 3))->addText('และ');
-		
+		$table->addCell(4000)->addText('         นายสีหศักดิ์ พวงเกตุแก้ว');
+		$table->addCell(3000)->addText('ปลัดกระทรวงการต่างประเทศ');
+		$table->addCell(3000)->addText('ผู้รับคำรับรอง');
 		
 		$table->addRow();
-		$table->addCell(6000)->addText('นายวิชาวัฒน์ อิศรภัคดี');
-		$table->addCell(6000)->addText('รองปลัดกระทรวงต่างประเทศ');
-		$table->addCell(6000)->addText('ผู้ทำคำรับรอง');
+		$table->addCell(180000, array('gridSpan' => 3))->addText('และ',null,array('align'=>'center'));
+		
 		
 		$table->addRow();
-		$table->addCell(6000)->addText('นายศรัณย์ เจริญสุวรรณ');
-		$table->addCell(6000)->addText('อธิบดีกรมยุโรป');
-		$table->addCell(6000)->addText('ผู้รับคำรับรอง');
+		$table->addCell(4000)->addText('         นายวิชาวัฒน์ อิศรภัคดี');
+		$table->addCell(3000)->addText('รองปลัดกระทรวงต่างประเทศ');
+		$table->addCell(3000)->addText('ผู้ทำคำรับรอง');
 		
-		$section->addText('2. คำรับรองนี้เป็นคำรับรองฝ่ายเดียว มิใช่สัญญาและใช้สำหรับระยะเวลา 1 ปี เริ่มตั้งแต่วันที่');
-		$section->addText('    1 ตุลาคม 2555 ถึงวันที่ 30 กันยายน 2556');
+		$table->addRow();
+		$table->addCell(4000)->addText('         นายศรัณย์ เจริญสุวรรณ');
+		$table->addCell(3000)->addText('อธิบดีกรมยุโรป');
+		$table->addCell(3000)->addText('ผู้รับคำรับรอง');
 		
-		$section->addText('3. รายละเอียดของคำรับรอง ได้แก่ กรอบการประเมินผล ประเด็นการประเมินผล การปฏิบัติราชการ');
-		$section->addText('    น้ำหนัก ตัวชี้วัดผลการปฏิบัติราชการ เป้าหมาย เกณฑ์การให้คะแนน และรายละเอียดอื่น ๆ');
-		$section->addText('    ตามที่ปรากฏอยู่ในเอกสารประกอบท้ายคำรับรองนี้');
+		$section->addText('');
 		
-		$section->addText('4. ข้าพเจ้า นายสีหศักดิ์ พวงเกตุแก้ว ในฐานะปลัดกระทรวงต่างประเทศและผู้บังคับบัญชาของ');
-		$section->addText('    นายวิชาวัฒน์ อิศรภัคดี รองปลัดกระทรวงที่กำกับดูแลกรมยุโรป และนายศรัณย์ เจริญสุวรรณ');
-		$section->addText('    อธิบดีกรมยุโรป ได้พิจารณาและเห็นชอบกับแผนปฏิบัติราชการและแนวทางการการพัฒนาการปฏิบัติ');
-		$section->addText('    ราชการ เป้าหมาย เกณฑ์การให้คะแนน และรายละเอียดอื่น ๆ ตามที่กำหนดในเอกสารประกอบ');
-		$section->addText('    ท้ายคำรับรองนี้ และข้าพเจ้ายินดีจะให้คำแนะนำกำกับ และตรวจสอบผลการปฏิบัติราชการของ');
-		$section->addText('    นายวิชาวัฒน์ อิศรภัคดี และนายศรัณย์ เจริญสุวรรณ ให้เป็นไปตามคำรับรองที่จัดทำขึ้นนี้');
+		$section->addText('2. คำรับรองนี้เป็นคำรับรองฝ่ายเดียว มิใช่สัญญาและใช้สำหรับระยะเวลา 1 ปี เริ่มตั้งแต่วันที่',null,'TextShortStyle');
+		$section->addText('    1 ตุลาคม 2555 ถึงวันที่ 30 กันยายน 2556',null,'TextShortStyle');
 		
-		$section->addText('5. ข้าพเจ้า นายวิชาวัฒน์ อิศรภัคดี รองปลัดกระทรวงต่างประเทศที่กำกับดูแลกรมยุโรป และ');
-		$section->addText('    นายศรัณย์ เจริญสุวรรณ อธิบดีกรมยุโรป ได้ทำความเข้าใจคำรับรองตาม 3 แล้ว ขอให้คำรับรองกับ');
-		$section->addText('    ปลัดกระทรวงต่างประเทศ ว่าจะมุ่งมั่นปฏิบัติราชการให้เกิดผลงานที่ดีตามเป้าหมายของตัวชี้วัด');
-		$section->addText('    แต่ละตัวในระดับสูงสุด เพื่อให้เกิดประโยชน์สุขแก่ประชาชน ตามที่ให้คำรับรองไว้');
-		$section->addText('    ');
+		$section->addText('');
 		
-		$section->addText('6. ผู้รับคำรับรองและผู้ทำคำรับรองได้เข้าใจคำรับรองการปฏิบัติราชการและเห็นพ้องกันแล้ว');
-		$section->addText('    จึงได้ลงลายมือชื่อไว้เป็นสำคัญ');
+		$section->addText('3. รายละเอียดของคำรับรอง ได้แก่ กรอบการประเมินผล ประเด็นการประเมินผลการปฏิบัติราชการ',null,'TextShortStyle');
+		$section->addText('    น้ำหนัก ตัวชี้วัดผลการปฏิบัติราชการ เป้าหมาย เกณฑ์การให้คะแนน และรายละเอียดอื่น ๆ',null,'TextShortStyle');
+		$section->addText('    ตามที่ปรากฏอยู่ในเอกสารประกอบท้ายคำรับรองนี้',null,'TextShortStyle');
+		
+		$section->addText('');
+		
+		$section->addText('4. ข้าพเจ้า นายสีหศักดิ์ พวงเกตุแก้ว ในฐานะปลัดกระทรวงการต่างประเทศและผู้บังคับบัญชาของ',null,'TextShortStyle');
+		$section->addText('    นายวิชาวัฒน์ อิศรภัคดี รองปลัดกระทรวงที่กำกับดูแลกรมยุโรป และนายศรัณย์ เจริญสุวรรณ',null,'TextShortStyle');
+		$section->addText('    อธิบดีกรมยุโรป ได้พิจารณาและเห็นชอบกับแผนปฏิบัติราชการและแนวทางการพัฒนาการปฏิบัติ',null,'TextShortStyle');
+		$section->addText('    ราชการของกรมยุโรป ประเด็นการประเมิลผลการปฏิบัติราชการ น้ำหนัก ตัวชี้วัดผลการปฏิบัติ',null,'TextShortStyle');
+		$section->addText('    ราชการ เป้าหมาย เกณฑ์การให้คะแนน และรายละเอียดอื่น ๆ ตามที่กำหนดในเอกสารประกอบ',null,'TextShortStyle');
+		$section->addText('    ท้ายคำรับรองนี้ และข้าพเจ้ายินดีจะให้คำแนะนำกำกับ และตรวจสอบผลการปฏิบัติราชการของ',null,'TextShortStyle');
+		$section->addText('    นายวิชาวัฒน์ อิศรภัคดี และนายศรัณย์ เจริญสุวรรณ ให้เป็นไปตามคำรับรองที่จัดทำขึ้นนี้',null,'TextShortStyle');
+		
+		$section->addText('');
+		
+		$section->addText('5. ข้าพเจ้า นายวิชาวัฒน์ อิศรภัคดี รองปลัดกระทรวงการต่างประเทศที่กำกับดูแลกรมยุโรป และ',null,'TextShortStyle');
+		$section->addText('    นายศรัณย์ เจริญสุวรรณ อธิบดีกรมยุโรป ได้ทำความเข้าใจคำรับรองตาม 3 แล้ว ขอให้คำรับรองกับ',null,'TextShortStyle');
+		$section->addText('    ปลัดกระทรวงการต่างประเทศ ว่าจะมุ่งมั่นปฏิบัติราชการให้เกิดผลงานที่ดีตามเป้าหมายของตัวชี้วัด',null,'TextShortStyle');
+		$section->addText('    แต่ละตัวในระดับสูงสุด เพื่อให้เกิดประโยชน์สุขแก่ประชาชน ตามที่ให้คำรับรองไว้',null,'TextShortStyle');
+		
+		$section->addText('');
+		
+		$section->addText('6. ผู้รับคำรับรองและผู้ทำคำรับรองได้เข้าใจคำรับรองการปฏิบัติราชการและเห็นพ้องกันแล้ว',null,'TextShortStyle');
+		$section->addText('    จึงได้ลงลายมือชื่อไว้เป็นสำคัญ',null,'TextShortStyle');
+		
+		$section->addTextBreak(2);
+		$st=array('spacing'=>0,'spaceBefore'=>0,'spaceAfter'=>0,'align'=>'center');
+		$table = $section->addTable();
+		$table->addRow();
+		$table->addCell(4600)->addText('…………………………………………………',null,$st);
+		$table->addCell(4600)->addText('…………………………………………………',null,$st);
+
+		$table->addRow();
+		$table->addCell(4600)->addText('( นายสีหศักดิ์ พวงเกตุแก้ว )',null,$st);
+		$table->addCell(4600)->addText('( นายวิชาวัฒน์ อิศรภัคดี )',null,$st);
+		
+		$table->addRow();
+		$table->addCell(4600)->addText('ปลัดกระทรวงการต่างประเทศ',null,$st);
+		$table->addCell(4600)->addText('รองปลัดกระทรวงการต่างประเทศ',null,$st);
 		
 		
+		$table->addRow();
+		$table->addCell(4600)->addText('วันที่ .........................................',null,$st);
+		$table->addCell(4600)->addText('วันที่ .........................................',null,$st);
+		
+		$section->addTextBreak(1);
+		$table = $section->addTable();
+		$table->addRow();
+		$table->addCell(4600)->addText('…………………………………………………',null,$st);
+
+		$table->addRow();
+		$table->addCell(4600)->addText('( นายศรัณย์ เจริญสุวรรณ )',null,$st);
+		
+		$table->addRow();
+		$table->addCell(4600)->addText('อธิบดีกรมยุโรป',null,$st);
+		
+		
+		$table->addRow();
+		$table->addCell(4600)->addText('วันที่ .........................................',null,$st);
 		
 		$file_name=uniqid('managewarranty',true).'.docx';
 		$path='docs/word'.$file_name;
