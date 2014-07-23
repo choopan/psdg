bd405f1a0f13f26c3ba102a8f3ab7767314a56ff
boy338 <boy.338@hotmail.com>
2014-07-24 02:50:29 +0700

    menu

--- application/views/menu.php
+++ application/views/menu.php
@@ -46,7 +46,7 @@
 						<li>
                             <a href="<?php echo site_url("main/changeyear"); ?>"><i class="fa fa-calendar fa-fw"></i> ปีงบประมาณ <strong><u><?php echo $this->session->userdata('sessyear'); ?></u></strong></a>
                         </li>
-                        
+                       
                         <?php	if($this->session->userdata('sessadmin_min') == 1) { ?>
                         <li>
                             <a href=""><i class="fa fa-bar-chart-o fa-fw"></i> ผู้ดูแลตัวชี้วัด <span class="fa arrow"></span></a>
